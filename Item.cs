28b619e1fbdb4979b78cb813737f343996343400
SantiagoMatzkin <santiago.matzkin@gmail.com>
2025-10-22 20:39:49 +0200

    new: Item class

--- Item.cs
+++ Item.cs
@@ -2,6 +2,37 @@ namespace WorldOfZuul
 {
     public class Item
     {
-        
+        private string name;
+        private string description;
+        private float weight; // Just in case we need it
+        private float value;
+
+        public Item(string Name, string Description, float Weight, float Value)
+        {
+            name = Name;
+            description = Description;
+            weight = Weight;
+            value = Value;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public string getDescription()
+        {
+            return description;
+        }
+
+        public float getWeight()
+        {
+            return weight;
+        }
+
+        public float getValue()
+        {
+            return value;
+        }
     }
 }
