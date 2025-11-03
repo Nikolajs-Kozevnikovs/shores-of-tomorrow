972e74a1d6fe5a582903f78163cbe3d750ba065a
Nikolajs Kozevnikovs <nikolajs.kozevnikovs@gmail.com>
2025-11-03 17:31:30 +0100

    Refactor Quest class properties and comments
    Refactor Quest class by adding nullable properties for Outcomes and QuestGiver, and update comments for clarity.

--- Quest.cs
+++ Quest.cs
@@ -2,17 +2,18 @@ using System.Reflection.Metadata.Ecma335;
 
 namespace WorldOfZuul 
 {
-  public enum QuestState { Pending, Completed, Done, Active, Locked };
+  public enum QuestState { Pending, Completed, Done, Active, Locked }; // no state for yet unavailable quests
 
   public class Quest {
     public string Name { get; private set; }
     public string Description { get; }
-    public QuestState State { get; set; }
-    // tbd
-    public Dictionary<string, Quest> Outcomes { get; private set; }
-    public string Objective { get; } // target progress
+    public string Objective { get; } // for now it is not exactly clear how to use this
     public List<int[]> ActiveTiles { get; private set; } // array of coordinates for the tiles where the quest can be completed
-    //public NPC QuestGiver { get; private set; }
+    
+    public QuestState State { get; set; }
+    
+    public Dictionary<string, Quest>? Outcomes;
+    public NPC? QuestGiver;
 
     public Quest(string name, string description, string objective, List<int[]> activeTiles ) // outcomes are cut =, fix later
     {
@@ -20,8 +21,6 @@ namespace WorldOfZuul
       Description = description;
       Objective = objective;
       ActiveTiles = activeTiles;
-      // Outcomes = outcomes ?? new Dictionary<string, Quest>();
-      //QuestGiver = questGiver;
     }
 
     public void Accept()
@@ -49,6 +48,14 @@ namespace WorldOfZuul
 
         o.Value.State = QuestState.Locked;
       }
+    }
+
+    public void setQuestGiver(NPC newNPC) {
+      questGiver = newNPC;
+    }
+
+    public void setOutcomes(Dictionary<string, Quest> newOutcomes) {
+      outcomes = newOutcomes;
     }
   }
 }
