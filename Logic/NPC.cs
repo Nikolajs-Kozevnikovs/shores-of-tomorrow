2edbd0cbb72bf2a8e72643035d5b3645f3718020
Nikolai Kozhevnikov <nikolajs.kozevnikovs@gmail.com>
2025-12-05 15:34:30 +0100

    struct: moved files to match three layer architecture

--- Logic/NPC.cs
+++
@@ -1,8 +0,0 @@
-public class NPC
-{
-    public string Name { get; set; }
-    public int X { get; set; }  // Room grid coordinates
-    public int Y { get; set; }
-    public List<string> QuestsGiven { get; set; } = new List<string>();
-    public List<string> QuestsFinishable { get; set; } = new List<string>();
-}
