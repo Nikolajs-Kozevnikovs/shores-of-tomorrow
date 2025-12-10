0021a330c37ec579eafaba1ccc23d94c784ddd1d
Nikolai Kozhevnikov <nikolajs.kozevnikovs@gmail.com>
2025-12-10 18:20:12 +0200

    data: added rooms and NPCs

--- src/Logic/NPC.cs
+++ src/Logic/NPC.cs
@@ -2,17 +2,19 @@ namespace WorldOfZuul.Logic;
 public class NPC
 {
     public string Name { get; set; }
+    public string Profession { get; set; }
     public int X { get; set; }  // Room grid coordinates
     public int Y { get; set; }
     public List<string> QuestsGiven { get; set; } = new List<string>();
     public List<string> QuestsFinishable { get; set; } = new List<string>();
 
-    public NPC(string name, int x, int y, List<string> questsGiven, List<string> questsFinishable)
+    public NPC(string name, string profession, int x, int y, List<string> questsGiven, List<string> questsFinishable)
     {
         Name = name;
         X = x;
         Y = y;
         QuestsGiven = questsGiven;
         QuestsFinishable = questsFinishable;
+        Profession = profession;
     }
 }
