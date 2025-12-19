d66acb70055a96de55804dc8ec726950baf4258a
Nikolai Kozhevnikov <nikolajs.kozevnikovs@gmail.com>
2025-12-19 13:18:27 +0200

    removed NPC property questsFinisheable

--- src/Logic/NPC.cs
+++ src/Logic/NPC.cs
@@ -6,15 +6,13 @@ public class NPC
     public int X { get; set; }  // Room grid coordinates
     public int Y { get; set; }
     public List<string> QuestsGiven { get; set; } = new List<string>();
-    public List<string> QuestsFinishable { get; set; } = new List<string>();
 
-    public NPC(string name, string profession, int x, int y, List<string> questsGiven, List<string> questsFinishable)
+    public NPC(string name, string profession, int x, int y, List<string> questsGiven)
     {
         Name = name;
         X = x;
         Y = y;
         QuestsGiven = questsGiven;
-        QuestsFinishable = questsFinishable;
         Profession = profession;
     }
 }
